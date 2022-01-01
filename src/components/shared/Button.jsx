c450311e60723b7a90efa72de0c290828ac69c71
Amir Ahmadi <eamahma@gmail.com>
2021-12-31 20:39:52 -0500

    button disabled bug resolved

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Button({children, version, type, isDisabled}) {
   return (
-    <button type={type} disable={isDisabled} className={`btn btn-${version}`}>
+    <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
       {children}
     </button>
   )
@@ -12,7 +12,7 @@ function Button({children, version, type, isDisabled}) {
 Button.defaultProps = {
   version: 'primary',
   type: 'button',
-  isDisabled: false,
+  isDisabled: false
 }
 
 Button.propTypes = {
